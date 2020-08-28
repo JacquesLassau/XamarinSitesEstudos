4153e8fd79b077db17d723f6c32a441d452fe677
JacquesLassau <37740292+JacquesLassau@users.noreply.github.com>
2020-08-28 14:27:54 -0300

    Conhecendo o Xamarin - App com Links Externos

---
+++ XamarinSitesEstudos/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace XamarinSitesEstudos
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void OnTapGestureRecognizerTapped_InstitutoTim(object sender, EventArgs e)
+        {
+            Launcher.OpenAsync("https://cursos.timtec.com.br/courses");            
+        }
+
+        public void OnTapGestureRecognizerTapped_CodeCademy(object sender, EventArgs e)
+        {
+            Launcher.OpenAsync("https://www.codecademy.com/");
+        }
+
+        public void OnTapGestureRecognizerTapped_Udacity(object sender, EventArgs e)
+        {
+            Launcher.OpenAsync("https://br.udacity.com/course/java-programming-basics--ud282");
+        }
+
+        public void OnTapGestureRecognizerTapped_Udemy(object sender, EventArgs e)
+        {
+            Launcher.OpenAsync("https://www.udemy.com/pt/courses/free/");
+        }
+    }
+}
